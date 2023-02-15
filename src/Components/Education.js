23cbea8c2d07f2e1f1da1d2ef83656e4dfb85fb4
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-15 21:04:15 +0200

    Home Page improved visually

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -12,36 +12,39 @@ export default function Education(props) {
 
     
     <header>
-        
-        <div className="form">
-        <h1>Education</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="upper">
-                    <div className="schoolName">
-                        <label htmlFor="schoolName">School Name</label>
-                        <input type="text" placeholder='School Name...' required
-                        onChange={props.handleChange} id='school' value={props.school} />   
-                    </div>
-                    <div className="titleOfStudy">
-                        <label htmlFor="titleOfStudy">Title of study</label>
-                        <input type="text" placeholder='Title of study...' required
-                        onChange={props.handleChange} id='studyTitle' value={props.titleOfStudy}  />
-                    </div>
-                </div> 
-                <div className="lower">  
-                    <div className="from">
-                        <label htmlFor="from">From</label>
-                        <input type="date"  required  
-                         onChange={props.handleChange} id='fromDate' value={props.fromDate}  />
-                    </div>
-                    <div className="to">
-                        <label htmlFor="to">To</label>
-                        <input type="date"  required  
-                         onChange={props.handleChange} id='toDate' value={props.toDate} />
-                    </div>
-                </div>  
-                 
-            </form>
+            
+        <div className="card">
+            <h1>Education</h1>
+            <div className="card-box">
+                <form onSubmit={handleSubmit}>
+                    <div className="upper">
+                        <div className="schoolName">
+                            <label htmlFor="schoolName">School Name</label>
+                            <input type="text" placeholder='School Name...' required
+                            onChange={props.handleChange} id='school' value={props.school} />   
+                        </div>
+                        <div className="titleOfStudy">
+                            <label htmlFor="titleOfStudy">Title of study</label>
+                            <input type="text" placeholder='Title of study...' required
+                            onChange={props.handleChange} id='studyTitle' value={props.titleOfStudy}  />
+                        </div>
+                    </div> 
+                    <div className="lower">  
+                        <div className="from">
+                            <label htmlFor="from">From</label>
+                            <input type="date"  required  
+                            onChange={props.handleChange} id='fromDate' value={props.fromDate}  />
+                        </div>
+                        <div className="to">
+                            <label htmlFor="to">To</label>
+                            <input className='date' type="date"  required  
+                            onChange={props.handleChange} id='toDate' value={props.toDate} />
+                        </div>
+                    </div>  
+                    
+                </form>
+            </div>
+            
         </div>
     </header>
   )
--- src/Components/Jobs.js
+++ src/Components/Jobs.js
@@ -10,41 +10,44 @@ export default function Jobs(props) {
   return (
     <header>
         
-        <div className="form">
-        <h1>Exprerience</h1>
-            <form onSubmit={handleSubmit} >
-                <div className="upper">
-                    <div className="company">
-                        <label htmlFor="company">Company Name</label>
-                        <input type="text" placeholder='Company Name...' required
-                        onChange={props.handleChange} id="company" value={props.company} />   
+        <div className="card">
+            <h1>Exprerience</h1>
+            <div className="card-box">
+                <form onSubmit={handleSubmit} >
+                    <div className="upper">
+                        <div className="company">
+                            <label htmlFor="company">Company Name</label>
+                            <input type="text" placeholder='Company Name...' required
+                            onChange={props.handleChange} id="company" value={props.company} />   
+                        </div>
+                        <div className="position">
+                            <label htmlFor="position">Position</label>
+                            <input type="text" placeholder='Position...' required
+                            onChange={props.handleChange} id="position"  value={props.position} />
+                        </div>
+                    </div> 
+                    <div className="lower">  
+                        <div className="from">
+                            <label htmlFor="from">From</label>
+                            <input type="date"  required 
+                            onChange={props.handleChange} id="fromDateJob"  value={props.fromJobDate} />
+                        </div>
+                        <div className="to">
+                            <label htmlFor="to">To</label>
+                            <input type="date"  required  
+                            onChange={props.handleChange} id="ToDateJob" value={props.toJobDate} />
+                        </div>
                     </div>
-                    <div className="position">
-                        <label htmlFor="position">Position</label>
-                        <input type="text" placeholder='Position...' required
-                        onChange={props.handleChange} id="position"  value={props.position} />
-                    </div>
-                </div> 
-                <div className="lower">  
-                    <div className="from">
-                        <label htmlFor="from">From</label>
-                        <input type="date"  required 
-                        onChange={props.handleChange} id="fromDateJob"  value={props.fromJobDate} />
-                    </div>
-                    <div className="to">
-                        <label htmlFor="to">To</label>
-                        <input type="date"  required  
-                        onChange={props.handleChange} id="ToDateJob" value={props.toJobDate} />
-                    </div>
-                </div>
-                <div className="tasksContainer">
-                    <div className="tasks">
-                        <textarea type="text" placeholder='Enter Tasks here...' 
-                        onChange={props.handleChange} id="Tasks" value={props.tasks} />
-                    </div>
-                </div>  
-              
-            </form>
+                    <div className="tasksContainer">
+                        <div className="tasks">
+                            <textarea type="text" placeholder='Enter Tasks here...' 
+                            onChange={props.handleChange} id="Tasks" value={props.tasks} />
+                        </div>
+                    </div>  
+                
+                </form>
+            </div>
+            
         </div>
     </header>
   )
